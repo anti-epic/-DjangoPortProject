55f0f11f6e7a182e7476816d647280be35c70038
anti-epic <donahoe.chase@gmail.com>
2023-05-05 12:34:48 -0500

    installed gunicorn

--- backend/views.py
+++ backend/views.py
@@ -7,8 +7,10 @@ from .models import Form
 #building react views
 from django.views.generic import TemplateView
 from django.conf import settings
+from django.urls import path
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 class ReactAppView(TemplateView):
     template_name = "index.html"
@@ -21,9 +23,9 @@ class ReactAppView(TemplateView):
 application = WhiteNoise(get_wsgi_application())
 application.add_files(settings.STATIC_ROOT, prefix=settings.STATIC_URL)
 
-urlpatterns += staticfiles_urlpatterns()
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += [path("", ReactAppView.as_view(), name="react_app")]
+# urlpatterns += staticfiles_urlpatterns()
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += [path("", ReactAppView.as_view(), name="react_app")]
 
 
 # Create your views here.
