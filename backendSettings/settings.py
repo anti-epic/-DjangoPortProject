af71dc6d42f97a4864b1ede5a20800406c0ea7de
anti-epic <donahoe.chase@gmail.com>
2023-05-05 13:28:09 -0500

    disable debug

--- backendSettings/settings.py
+++ backendSettings/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yw-fwjv=cpd$$ujy4^l@%w$s^elf7djxi0q-(9#p7d@*m78vo4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
