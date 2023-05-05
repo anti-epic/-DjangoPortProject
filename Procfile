e1805d42df4fe2ddfe522358f4b057dba826701c
anti-epic <donahoe.chase@gmail.com>
2023-05-05 12:14:22 -0500

    front and backend connected, also setup build for heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn <django-project-name>.wsgi
--- backend/views.py
+++ backend/views.py
@@ -2,6 +2,30 @@ from django.shortcuts import render
 from rest_framework import generics
 from .serializers import FormSerializer
 from .models import Form
+
+
+#building react views
+from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from whitenoise import WhiteNoise
+class ReactAppView(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["DEBUG"] = settings.DEBUG
+        return context
+
+application = WhiteNoise(get_wsgi_application())
+application.add_files(settings.STATIC_ROOT, prefix=settings.STATIC_URL)
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [path("", ReactAppView.as_view(), name="react_app")]
+
+
 # Create your views here.
 
 class FormView(generics.ListAPIView):
