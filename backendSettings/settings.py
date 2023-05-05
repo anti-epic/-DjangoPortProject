ac89f7d715124a0bc570fac89e97f3a6e0a85b75
anti-epic <donahoe.chase@gmail.com>
2023-05-05 13:31:39 -0500

    added osrs-project to install apps

--- backendSettings/settings.py
+++ backendSettings/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'backend.apps.PollsConfig',
+    'osrs-project',
     # 'backend.apps.backendConfig',
     'rest_framework',
     'django.contrib.admin',
