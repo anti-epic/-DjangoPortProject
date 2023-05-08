25923ff274bdc0db51a1cc32571f6dac10ac8e1b
anti-epic <donahoe.chase@gmail.com>
2023-05-08 17:03:16 -0500

    adjusted build command and procfile

--- backendSettings/settings.py
+++ backendSettings/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',                              # add this
-    'todo',
+    # 'todo',
 ]
 
 MIDDLEWARE = [
