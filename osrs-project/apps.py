3d08d96ac983efcbecfd7e68b1ee67f5c5efc834
anti-epic <donahoe.chase@gmail.com>
2023-05-08 15:49:25 -0500

    adjusted build command and procfile

--- osrs-project/apps.py
+++ osrs-project/apps.py
@@ -1,10 +1,10 @@
 from django.apps import AppConfig
 
-print('Loading backend.apps')
+# print('Loading backend.apps')
 
 # class backendConfig(AppConfig):
 #     default_auto_field = 'django.db.models.BigAutoField'
 #     name = 'backend'
-class PollsConfig(AppConfig):
+class OsrsProjectConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'backend'
+    name = 'osrs-project'
