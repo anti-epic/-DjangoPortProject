1f1457ef4780ffb5dcfda1923ab4e007916903ca
anti-epic <donahoe.chase@gmail.com>
2023-05-08 22:07:20 -0500

    adjusted build command and procfile

--- backendSettings/settings.py
+++ backendSettings/settings.py
@@ -29,7 +29,7 @@ STATIC_URL = "/static/"
 # STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend', 'build', 'static'),
+   os.path.join(BASE_DIR, 'frontend/build', 'static')
 ]
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 # STATIC_URL = '/static/'
