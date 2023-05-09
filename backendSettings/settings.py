da80f6b60a744126289d19e31983aeb4ef20f025
anti-epic <donahoe.chase@gmail.com>
2023-05-09 18:19:06 -0500

    added signup logic

--- backendSettings/settings.py
+++ backendSettings/settings.py
@@ -23,7 +23,8 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 REACT_APP_DIR = os.path.join(BASE_DIR, 'frontend')
 
 STATICFILES_DIRS = [
-    os.path.join(REACT_APP_DIR, 'build', 'static'),
+    os.path.join(REACT_APP_DIR, 'build'),
+
 ]
 
 
