45e64f0fb5705dbaeced000d5e57885632bad19f
anti-epic <donahoe.chase@gmail.com>
2023-05-09 18:05:05 -0500

    added signup logic

--- backendSettings/settings.py
+++ backendSettings/settings.py
@@ -24,7 +24,6 @@ REACT_APP_DIR = os.path.join(BASE_DIR, 'frontend')
 
 STATICFILES_DIRS = [
     os.path.join(REACT_APP_DIR, 'build', 'static'),
-    os.path.join(BASE_DIR, 'frontend', 'public'),
 ]
 
 
