5a6be0c8c1b5107cded3b5f2b30dfee6368220d1
anti-epic <donahoe.chase@gmail.com>
2023-05-09 18:35:43 -0500

    added signup logic

--- backendSettings/settings.py
+++ backendSettings/settings.py
@@ -19,11 +19,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 IS_HEROKU = "DYNO" in os.environ
 STATIC_URL = "/static/"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 REACT_APP_DIR = os.path.join(BASE_DIR, 'frontend')
 
 STATICFILES_DIRS = [
-    os.path.join(REACT_APP_DIR, 'build', 'static'),
+    os.path.join(STATIC_ROOT, 'build', 'static'),
 
 ]
 
