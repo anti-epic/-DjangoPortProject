eb74c8a4a831a9ec56e9d52cafd5185364fb7250
anti-epic <donahoe.chase@gmail.com>
2023-05-08 20:32:56 -0500

    adjusted build command and procfile

--- backendSettings/wsgi.py
+++ backendSettings/wsgi.py
@@ -7,11 +7,18 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
-import os
-from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
+# import os
+# from django.core.wsgi import get_wsgi_application
+# from whitenoise import WhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "reactdjango.settings")
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "reactdjango.settings")
+
+# application = get_wsgi_application()
+# application = WhiteNoise(application)
+# import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backendSettings.settings")
+
+from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
-application = WhiteNoise(application)
